d656956f7ee50a127d2c2a000e27ae2c2f96bb8b
Ayana File <ayanafileorg@gmail.com>
2025-12-20 22:51:11 +0300

    solved cors poly

--- linkjournal_backend/main.go
+++ linkjournal_backend/main.go
@@ -59,6 +59,7 @@ func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 
 		// Replace "http://localhost:3000" with "*" if you want to allow ANY frontend to connect
+		// this help team to access the locaalhost
 		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
